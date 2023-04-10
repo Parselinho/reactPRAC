f62166118125e7798a5c6c61f6c65f1f0a0375c4
Gal Parselani <Parselinho@gmail.com>
2023-04-10 19:30:54 +0300

    Create NotFound.js

---
+++ reactprac/src/countries/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <h2>404 - Not Found</h2>
+      <p>The search did not return any results.</p>
+    </div>
+  );
+}
+
+export default NotFound;
