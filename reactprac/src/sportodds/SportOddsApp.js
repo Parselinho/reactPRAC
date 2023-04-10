7358c13c1eab60091aa866f110be090b8ab773be
Gal Parselani <Parselinho@gmail.com>
2023-04-10 20:16:23 +0300

    Update SportOddsApp.js

--- reactprac/src/sportodds/SportOddsApp.js
+++ reactprac/src/sportodds/SportOddsApp.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+
 import getUpcomingOdds from "./Api";
+import RegionInput from "./RegionInput";
 import GameList from "./GameList";
 import OddsModal from "./OddsModal";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,16 +9,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const SportOddsApp = () => {
   const [data, setData] = useState(null);
   const [selectedGameIndex, setSelectedGameIndex] = useState(null);
+  const [region, setRegion] = useState("us");
   const apiKey = "ddbf5dd6e5msh030cf08a7275e29p1e5ceejsn2f17613e6446"; // Replace with your actual API key
 
   useEffect(() => {
     const fetchData = async () => {
-      const upcomingOdds = await getUpcomingOdds(apiKey);
+      const upcomingOdds = await getUpcomingOdds(apiKey, region);
       console.log("Fetched data:", upcomingOdds); // Log the fetched data
       setData(upcomingOdds);
     };
     fetchData();
-  }, []);
+  }, [region]);
 
   const handleGameClick = (index) => {
     setSelectedGameIndex(index);
@@ -34,16 +37,22 @@ const SportOddsApp = () => {
     setSelectedGameIndex((prevState) => prevState + 1);
   };
 
+  const handleRegionChange = (event) => {
+    setRegion(event.target.value);
+  };
+
   const selectedGame = data && data[selectedGameIndex];
 
   return (
     <div className="App">
       <h1>Upcoming Games</h1>
+      <RegionInput region={region} handleRegionChange={handleRegionChange} />
       {data ? (
         <GameList data={data} onGameClick={handleGameClick} />
       ) : (
         <p>Loading data...</p>
       )}
+
 
       {selectedGame && (
         <OddsModal
