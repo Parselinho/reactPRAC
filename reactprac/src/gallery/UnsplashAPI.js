445c1865cdf80c596759733d1dbda0d848172eca
Gal Parselani <Parselinho@gmail.com>
2023-04-10 20:55:43 +0300

    Update UnsplashAPI.js

--- reactprac/src/gallery/UnsplashAPI.js
+++ reactprac/src/gallery/UnsplashAPI.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const unsplashAPIKey = 'OUd_lQq6ithsvq9mikDzMHpv4Sbs8xoohKCoPWMuLQ4';
 
-export const searchPhotos = async (query, page, perPage) => {
+export const searchPhotos = async (query, page) => {
     try {
-      const response = await axios.get(`https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=${perPage}&client_id=${unsplashAPIKey}`);
+      const response = await axios.get(`https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=12&client_id=${unsplashAPIKey}`);
       console.log(response.data)
       return response.data;
     } catch (error) {
