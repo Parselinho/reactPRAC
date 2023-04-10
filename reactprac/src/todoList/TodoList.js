d201f741e079ed2bcc4ce860343ae9041a76f1cf
Gal Parselani <Parselinho@gmail.com>
2023-04-11 00:40:24 +0300

    Update TodoList.js

--- reactprac/src/todoList/TodoList.js
+++ reactprac/src/todoList/TodoList.js
@@ -1,46 +1,49 @@
 import React from 'react'
+import { Button, FormControl } from 'react-bootstrap'; // Import missing Bootstrap components
+import './css//Todolist.css';
+
 
 function TodoList(props) {
   return (
     <div>
       <ul className="list-group">
         {props.tasks.map((task, index) => (
-          <div key={task.id} className="list-group-item d-flex justify-content-between align-items-center">
+          <li key={task.id} className="list-group-item d-flex justify-content-between align-items-center">
             {props.editedTask?.id === task.id ? (
               <div>
-                <input
+                <FormControl
                   type="text"
                   value={props.editedTaskText}
                   onChange={props.onEditTaskTextChange}
                   autoFocus
                 />
-                <button
-                  className="btn btn-outline-success btn-sm ms-1"
+                <Button
+                  className="btn btn-outline-success btn-sm ms-1 white-text"
                   onClick={() => props.onUpdateTask(props.editedTask)}
                 >
                   Save
-                </button>
+                </Button>
               </div>
             ) : (
               <span>{task.text}</span>
             )}
             <div>
               {props.editedTask?.id === task.id ? null : (
-                <button
-                  className="btn btn-outline-secondary btn-sm me-1"
+                <Button
+                  className="btn btn-outline-secondary btn-sm me-1 white-text"
                   onClick={() => props.onEditTask(task)}
                 >
                   Edit
-                </button>
+                </Button>
               )}
-              <button
-                className="btn btn-outline-danger btn-sm"
+              <Button
+                className="btn btn-outline-danger btn-sm white-text"
                 onClick={() => props.onRemoveTask(task.id)}
               >
                 Remove
-              </button>
+              </Button>
             </div>
-          </div>
+          </li>
         ))}
       </ul>
     </div>
