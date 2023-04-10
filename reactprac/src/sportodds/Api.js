66d48d89140e8f7862b90ce00cc56a48effde3a0
Gal Parselani <Parselinho@gmail.com>
2023-04-10 20:16:27 +0300

    Update Api.js

--- reactprac/src/sportodds/Api.js
+++ reactprac/src/sportodds/Api.js
@@ -1,27 +1,23 @@
-// src/api.js
 import axios from "axios";
 
-const getUpcomingOdds = async (apiKey) => {
-  const options = {
-    method: "GET",
-    url: "https://odds.p.rapidapi.com/v4/sports/upcoming/odds",
-    params: {
-      regions: "us",
-      oddsFormat: "decimal",
-      markets: "h2h,spreads",
-      dateFormat: "iso",
-    },
-    headers: {
-      "X-RapidAPI-Key": apiKey,
-      "X-RapidAPI-Host": "odds.p.rapidapi.com",
-    },
-  };
-
+const getUpcomingOdds = async (apiKey, region) => {
   try {
-    const response = await axios.request(options);
+    const response = await axios.get("https://odds.p.rapidapi.com/v4/sports/upcoming/odds", {
+      params: {
+        regions: region,
+        oddsFormat: "decimal",
+        markets: "h2h,spreads",
+        dateFormat: "iso",
+      },
+      headers: {
+        "X-RapidAPI-Key": apiKey,
+        "X-RapidAPI-Host": "odds.p.rapidapi.com",
+      },
+    });
+
     return response.data;
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching data: ", error);
     return null;
   }
 };
